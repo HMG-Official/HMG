45f6e1443c0151043967ed2f3ffda0686337fad1
asistex <asistex@yahoo.com.ar>
2020-10-09 16:42:46 -0300

    upd2

--- libHmgUni.bat
+++ libHmgUni.bat
@@ -1,5 +1,6 @@
 @echo off
 
+echo step 1
 set previouspath=%path%
 set path=c:\bcd\prg\comp\mgw102\bin;c:\bcd\prg\pcomp\hb32_mgw102\bin;%path%
 :: set HMGPATH=%~dp0
@@ -8,6 +9,7 @@ set path=c:\bcd\prg\comp\mgw102\bin;c:\bcd\prg\pcomp\hb32_mgw102\bin;%path%
 echo.
 echo. Building HMG Library Unicode 32b
 echo.
+echo step 2
 if not exist "INCLUDE\_UNICODE.ch" ( goto ERROR2 )
 copy INCLUDE\_UNICODE.ch  INCLUDE\SET_COMPILE_HMG_UNICODE.ch > nul
 if %ERRORLEVEL% NEQ 0 goto ERROR2
@@ -15,10 +17,12 @@ goto BUILDLIB
 
 
 :ERROR2
+echo step 3
 echo. ERROR: file (INCLUDE\_UNICODE.ch) not exist or failed copy
 exit
 
 
 :BUILDLIB
-c:\bcd\prg\comp\mgw102\bin\hbmk2 hmg32.hbp
+echo step 4
+hbmk2 hmg32.hbp
 set path=%previouspath%
