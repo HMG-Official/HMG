b9db60f0e33813ebef71089f32cfd6b9954e4ffa
José M. C. Quintas <jmcquintas@gmail.com>
2021-01-30 08:12:18 -0300

    all compilers and 32/64 bits at same time

--- buildlib.bat
+++ buildlib.bat
@@ -3,9 +3,9 @@
 rem SYNTAX:  BuilLib.bat  [/nopause]  [/ansi] | [/unicode]
 
 if  "%1" == "/nopause" (
-    set nopause=1 
-) else ( 
-    set nopause=0 ) 
+    set nopause=1
+) else (
+    set nopause=0 )
 
 if "%1" == "/nopause" shift
 
@@ -21,21 +21,22 @@ echo. *********************************
 echo. *   HMG-Unicode Build Library   *
 echo. *********************************
 echo.
-echo. 
+echo.
 echo. Build HMG Library to support the character set:
 echo. -----------------------------------------------
 echo.
 echo. 1 - ANSI
-echo. 
-echo. 2 - UNICODE
-echo. 
-echo. 0 - Cancel
-echo. 
 echo.
-echo. 
-set /p option=Select an Option [0, 1, 2] (for default: UNICODE): 
-if %ERRORLEVEL% NEQ 0 goto OP2 
-if %option% == 1 goto OP1  
+echo. 2 - UNICODE
+echo.
+echo. 0 - Cancel
+echo.
+echo.
+echo.
+set /p option=Select an Option [0, 1, 2] (for default: UNICODE):
+if %ERRORLEVEL% NEQ 0 goto OP2
+set HB_COMPILER=mingw
+if %option% == 1 goto OP1
 if %option% == 2 goto OP2
 if %option% == 0 exit
 goto MENU
@@ -60,7 +61,7 @@ echo.
 title Build Library:         UNICODE
 echo. Build Library:         UNICODE
 echo.
-if not exist "INCLUDE\_UNICODE.ch" ( goto ERROR2 ) 
+if not exist "INCLUDE\_UNICODE.ch" ( goto ERROR2 )
 copy INCLUDE\_UNICODE.ch  INCLUDE\SET_COMPILE_HMG_UNICODE.ch > nul
 if %ERRORLEVEL% NEQ 0 goto ERROR2
 echo.UNICODE > hmglib.txt
@@ -85,8 +86,11 @@ exit
 
 :BUILDLIB
 SET HMGPATH=%~dp0
-SET PATH=%HMGPATH%\harbour\bin;%HMGPATH%\mingw\bin;%PATH%
-hbmk2 hmg32.hbp
+SET PATH=%HMGPATH%..\..\bin;%HMGPATH%..\..\comp\mingw32\bin;%PATH%
+
+SET HBMK_CMD=-q -m -n -L%PATH%
+
+hbmk2 hmg.hbp hmg.hbc
 echo.
 
-if %nopause% == 0 pause 
+if %nopause% == 0 pause
